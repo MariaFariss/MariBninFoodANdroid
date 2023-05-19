b206fb88b206420ce44cc9116ca524769d00cecd
MariaFariss <102377465+MariaFariss@users.noreply.github.com>
2023-05-19 21:40:37 +0200

    Account V0

--- app/src/main/java/com/example/maribninfood/ui/account/AccountViewModel.kt
+++ app/src/main/java/com/example/maribninfood/ui/account/AccountViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.maribninfood.ui.dashboard
+package com.example.maribninfood.ui.account
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class AccountViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
