0add00a59f470c568d87ccf829e0b3ab6ad81349
MariaFariss <102377465+MariaFariss@users.noreply.github.com>
2023-05-20 22:47:20 +0200

    bookmarks v0

--- app/src/main/java/com/example/maribninfood/ui/bookmarks/BookmarksFragment.kt
+++ app/src/main/java/com/example/maribninfood/ui/bookmarks/BookmarksFragment.kt
@@ -1,4 +1,4 @@
-package com.example.maribninfood.ui.notifications
+package com.example.maribninfood.ui.bookmarks
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.maribninfood.databinding.FragmentNotificationsBinding
+import com.example.maribninfood.databinding.FragmentBookmarksBinding
 
-class NotificationsFragment : Fragment() {
+class BookmarksFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentBookmarksBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,16 +22,16 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+//        val notificationsViewModel =
+//            ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentBookmarksBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val textView: TextView = binding.tvCategory
+//        notificationsViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
