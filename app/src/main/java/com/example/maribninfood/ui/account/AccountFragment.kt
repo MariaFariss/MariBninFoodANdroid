6eee6b51b7fa845110ef7193170c7621fb376197
MariaFariss <102377465+MariaFariss@users.noreply.github.com>
2023-05-20 21:55:03 +0200

    logout

--- app/src/main/java/com/example/maribninfood/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/maribninfood/ui/account/AccountFragment.kt
@@ -1,14 +1,18 @@
 package com.example.maribninfood.ui.account
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.maribninfood.LoginActivity
 import com.example.maribninfood.R
 import com.example.maribninfood.databinding.FragmentAccountBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class AccountFragment : Fragment() {
 
@@ -38,12 +42,42 @@ class AccountFragment : Fragment() {
 //        _binding = null
 //    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_account, container, false)
+//
+//    }
+override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+): View? {
+    val view = inflater.inflate(R.layout.fragment_account, container, false)
+    val logoutButton: ImageView = view.findViewById(R.id.logoutButton)
+    logoutButton.setOnClickListener {
+        logout()
     }
+
+    return view
+}
+
+
+    private fun logout() {
+        val intent = Intent(activity, LoginActivity::class.java)
+        startActivity(intent)
+//        activity?.finish() // Facultatif : fermer l'activité courante après la déconnexion
+    }
+//private fun logout() {
+//    // Déconnexion de l'utilisateur actuel
+//    FirebaseAuth.getInstance().signOut()
+//
+//    // Rediriger vers l'écran de connexion
+//    val intent = Intent(activity, LoginActivity::class.java)
+//    startActivity(intent)
+//    activity?.finish() // Facultatif : fermer l'activité courante après la déconnexion
+//}
 }
