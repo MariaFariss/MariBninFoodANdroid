7e36172a2583314b003727dc0f45d033cffbe5db
MariaFariss <102377465+MariaFariss@users.noreply.github.com>
2023-05-22 22:00:57 +0200

    layout account V2

--- app/src/main/java/com/example/maribninfood/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/maribninfood/ui/account/AccountFragment.kt
@@ -2,88 +2,85 @@ package com.example.maribninfood.ui.account
 
 import android.content.Intent
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import android.widget.ScrollView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.maribninfood.LoginActivity
 import com.example.maribninfood.MainActivity
 import com.example.maribninfood.R
-import com.example.maribninfood.databinding.FragmentAccountBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class AccountFragment : Fragment() {
 
-//    private var _binding: FragmentAccountBinding? = null
-//    private val binding get() = _binding!!
-//
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View {
-//        val dashboardViewModel =
-//            ViewModelProvider(this).get(AccountViewModel::class.java)
-//
-//        _binding = FragmentAccountBinding.inflate(inflater, container, false)
-//        val root: View = binding.root
-//
-////        val textView: TextView = binding.btnBackToHome
-////        dashboardViewModel.text.observe(viewLifecycleOwner) {
-////            textView.text = it
-////        }
-//        return root
-//    }
-//
-//    override fun onDestroyView() {
-//        super.onDestroyView()
-//        _binding = null
-//    }
+    private lateinit var personalInfoButton: TextView
+    private lateinit var myRecipesButton: TextView
+    private lateinit var personalInfoLayout: LinearLayout
+    private lateinit var myRecipesLayout: RecyclerView
+//    private lateinit var text: TextView
 
-//    override fun onCreateView(
-//        inflater: LayoutInflater,
-//        container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_account, container, false)
-//
-//    }
-override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-): View? {
-    val view = inflater.inflate(R.layout.fragment_account, container, false)
-    val logoutButton: ImageView = view.findViewById(R.id.logoutButton)
-    val goBack : ImageView=view.findViewById(R.id.arrowBack)
-    logoutButton.setOnClickListener {
-        logout()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_account, container, false)
+
+        personalInfoButton = view.findViewById(R.id.tv_personal_info_button)
+        myRecipesButton = view.findViewById(R.id.tv_my_recipes_button)
+
+        personalInfoLayout = view.findViewById(R.id.pesonelInfoLayout)
+        myRecipesLayout = view.findViewById(R.id.myrecipes_recyclerview)
+//        text = view.findViewById(R.id.textView)
+
+        personalInfoButton.setOnClickListener {
+            Log.d("AccountFragment", "personalInfoButton clicked")
+            personalInfoLayout.visibility = View.VISIBLE
+            myRecipesLayout.visibility = View.GONE
+            personalInfoButton.setTextColor(resources.getColor(R.color.white))
+            myRecipesButton.setTextColor(resources.getColor(R.color.grey))
+//            text.visibility = View.GONE
+        }
+
+        myRecipesButton.setOnClickListener {
+            Log.d("AccountFragment", "myRecipesButton clicked")
+            personalInfoLayout.visibility = View.GONE
+            myRecipesLayout.visibility = View.VISIBLE
+            personalInfoButton.setTextColor(resources.getColor(R.color.grey))
+            myRecipesButton.setTextColor(resources.getColor(R.color.white))
+//            text.visibility = View.VISIBLE
+        }
+
+        val logoutButton: ImageView = view.findViewById(R.id.logoutButton)
+        val goBack: ImageView = view.findViewById(R.id.arrowBack)
+        logoutButton.setOnClickListener {
+            logout()
+        }
+
+        goBack.setOnClickListener {
+            val intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        return view
     }
 
-    goBack.setOnClickListener(){
-        val intent = Intent(activity, MainActivity::class.java)
+    private fun logout() {
+        // Logout the current user
+        FirebaseAuth.getInstance().signOut()
+
+        // Redirect to the login screen
+        val intent = Intent(activity, LoginActivity::class.java)
         startActivity(intent)
+        activity?.finish() // Optional: Close the current activity after logout
     }
-
-    return view
-}
-
-//    private fun logout() {
-//        val intent = Intent(activity, LoginActivity::class.java)
-//        startActivity(intent)
-//    }
-
-private fun logout() {
-    // Déconnexion de l'utilisateur actuel
-    FirebaseAuth.getInstance().signOut()
-
-    // Rediriger vers l'écran de connexion
-    val intent = Intent(activity, LoginActivity::class.java)
-    startActivity(intent)
-    activity?.finish() // Facultatif : fermer l'activité courante après la déconnexion
-}
 }
