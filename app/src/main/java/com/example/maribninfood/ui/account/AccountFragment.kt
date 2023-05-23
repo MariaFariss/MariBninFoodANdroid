e4ab4138d9ab1033510c90d88aca2d1486314c43
MariaFariss <102377465+MariaFariss@users.noreply.github.com>
2023-05-23 23:52:03 +0200

    account edit personnel profile v3

--- app/src/main/java/com/example/maribninfood/ui/account/AccountFragment.kt
+++ app/src/main/java/com/example/maribninfood/ui/account/AccountFragment.kt
@@ -2,6 +2,7 @@ package com.example.maribninfood.ui.account
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Im
 import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.LayoutInflater
@@ -13,10 +14,12 @@ import android.widget.RelativeLayout
 import android.widget.ScrollView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.maribninfood.LoginActivity
 import com.example.maribninfood.MainActivity
 import com.example.maribninfood.R
+import com.example.maribninfood.editUserFragment
 import com.google.firebase.auth.FirebaseAuth
 
 class AccountFragment : Fragment() {
@@ -25,7 +28,10 @@ class AccountFragment : Fragment() {
     private lateinit var myRecipesButton: TextView
     private lateinit var personalInfoLayout: LinearLayout
     private lateinit var myRecipesLayout: RecyclerView
-//    private lateinit var text: TextView
+    private lateinit var mail: TextView
+    private lateinit var pseudo: TextView
+    private lateinit var editButton: ImageView
+
 
 
     override fun onCreateView(
@@ -35,12 +41,13 @@ class AccountFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_account, container, false)
 
+        //change the ui when the user click on the button
         personalInfoButton = view.findViewById(R.id.tv_personal_info_button)
         myRecipesButton = view.findViewById(R.id.tv_my_recipes_button)
 
         personalInfoLayout = view.findViewById(R.id.pesonelInfoLayout)
         myRecipesLayout = view.findViewById(R.id.myrecipes_recyclerview)
-//        text = view.findViewById(R.id.textView)
+
 
         personalInfoButton.setOnClickListener {
             Log.d("AccountFragment", "personalInfoButton clicked")
@@ -48,7 +55,7 @@ class AccountFragment : Fragment() {
             myRecipesLayout.visibility = View.GONE
             personalInfoButton.setTextColor(resources.getColor(R.color.white))
             myRecipesButton.setTextColor(resources.getColor(R.color.grey))
-//            text.visibility = View.GONE
+
         }
 
         myRecipesButton.setOnClickListener {
@@ -57,7 +64,25 @@ class AccountFragment : Fragment() {
             myRecipesLayout.visibility = View.VISIBLE
             personalInfoButton.setTextColor(resources.getColor(R.color.grey))
             myRecipesButton.setTextColor(resources.getColor(R.color.white))
-//            text.visibility = View.VISIBLE
+
+        }
+        // personal info
+        mail = view.findViewById(R.id.tv_mail)
+        pseudo = view.findViewById(R.id.tv_pseudo)
+        editButton = view.findViewById(R.id.edit)
+
+        // get the user info
+        val user = FirebaseAuth.getInstance().currentUser
+        user?.let {
+            // Name, email address, and profile photo Url
+            val email = user.email
+            val name = user.displayName
+//            val photoUrl = user.photoUrl
+            mail.text = email
+            pseudo.text = name
+        }
+        editButton.setOnClickListener {
+            findNavController().navigate(R.id.navigation_editPofile)
         }
 
         val logoutButton: ImageView = view.findViewById(R.id.logoutButton)
---
+++ app/src/main/java/com/example/maribninfood/ui/addRecipes/AddRecipesFragementViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.maribninfood.ui.addRecipes
+
+import androidx.lifecycle.ViewModel
+
+class AddRecipesFragementViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
